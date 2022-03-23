49f601773f394d3f5c8d6de64d62158b10adcd8f
Laurens <hflaurens@hotmail.com>
2022-03-23 20:21:48 +0100

    Improve logging of actions in base rest controllers

--- src/main/java/student/laurens/novibackend/controllers/BaseRestController.java
+++ src/main/java/student/laurens/novibackend/controllers/BaseRestController.java
@@ -48,39 +48,77 @@ public abstract class BaseRestController<R extends AbstractEntity> {
 
     abstract protected BaseService<R> getService();
 
+    protected void logProcessingStarted(final HttpMethod method) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] started.");
+    }
+
+    protected void logProcessingStarted(final HttpMethod method, final Integer resourceId) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with resourceId ["+resourceId+"] started");
+    }
+
+    protected void logProcessingStarted(final HttpMethod method, final String resourceName) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with resourceId ["+resourceName+"] started");
+    }
+
+    protected void logProcessingFinished(final HttpMethod method, final Integer resourceId) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with resourceId ["+resourceId+"] successfully finished.");
+    }
+
+    protected void logProcessingFinished(final HttpMethod method, final String resourceName) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with resourceId ["+resourceName+"] successfully finished.");
+    }
+
+    protected void logProcessingFinished(final HttpMethod method) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] successfully finished.");
+    }
+
     public ResponseEntity<Iterable<R>> get() {
+        logProcessingStarted(HttpMethod.GET);
+        logProcessingFinished(HttpMethod.GET);
         return new ResponseEntity<>(getService().getResources(), HttpStatus.OK);
     }
 
     public ResponseEntity<R> get(final String name) throws ResourceNotFoundException {
+        logProcessingStarted(HttpMethod.GET, name);
+        logProcessingFinished(HttpMethod.GET, name);
         return new ResponseEntity<>(getService().getResource(name), HttpStatus.OK);
     }
 
     public ResponseEntity<R> get(final Integer resourceId) throws ResourceNotFoundException {
+        logProcessingStarted(HttpMethod.GET, resourceId);
+
         getService().validateOwnershipOfResource(resourceId, HttpMethod.GET, getConsumer());
 
+        logProcessingFinished(HttpMethod.GET, resourceId);
         return new ResponseEntity<>(getService().getResourceById(resourceId), HttpStatus.OK);
     }
 
     public ResponseEntity<R> create(final R resource) {
+        logProcessingStarted(HttpMethod.POST);
+
         getService().createResource(resource);
 
+        logProcessingFinished(HttpMethod.POST);
         return new ResponseEntity<>(resource, HttpStatus.CREATED);
     }
 
     public ResponseEntity<R> update(final Integer resourceId, final R resource) throws ResourceNotFoundException, ResourceNotOwnedException {
-        getService().validateOwnershipOfResource(resourceId, HttpMethod.PUT, getConsumer());
+        logProcessingStarted(HttpMethod.PUT, resourceId);
 
+        getService().validateOwnershipOfResource(resourceId, HttpMethod.PUT, getConsumer());
         getService().updateResourceById(resourceId, resource);
 
+        logProcessingFinished(HttpMethod.PUT, resourceId);
         return new ResponseEntity<>(resource, HttpStatus.ACCEPTED);
     }
 
     public ResponseEntity<R> delete(final Integer resourceId) throws ResourceNotFoundException, ResourceNotOwnedException {
-        getService().validateOwnershipOfResource(resourceId, HttpMethod.DELETE, getConsumer());
+        logProcessingStarted(HttpMethod.DELETE, resourceId);
 
+        getService().validateOwnershipOfResource(resourceId, HttpMethod.DELETE, getConsumer());
         getService().deleteResourceById(resourceId);
 
+        logProcessingFinished(HttpMethod.DELETE, resourceId);
         return new ResponseEntity(createDeletedMessage(), HttpStatus.ACCEPTED);
     }
 
--- src/main/java/student/laurens/novibackend/controllers/ParentBaseRestController.java
+++ src/main/java/student/laurens/novibackend/controllers/ParentBaseRestController.java
@@ -19,8 +19,16 @@ public abstract class ParentBaseRestController<R extends AbstractEntity, P exten
 
     abstract protected ParentBaseService<P, R> getParentService();
 
+    protected void logProcessingStarted(final HttpMethod method, final Integer parentResourceId, final Integer resourceId) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"] started.");
+    }
+
+    protected void logProcessingFinished(final HttpMethod method, final Integer parentResourceId, final Integer resourceId) {
+        log.info("Processing ["+method+"] request on resource ["+getService().getResourceClass()+"] with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"] successfully finished.");
+    }
+
     public ResponseEntity<R> update(final Integer parentResourceId, final Integer resourceId, final R resource) throws ResourceNotFoundException, ResourceNotOwnedException {
-        log.info("Processing [PUT] request with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"] started");
+        logProcessingStarted(HttpMethod.PUT, parentResourceId, resourceId);
         P parentResource = getParentService().getResourceById(parentResourceId); // EXISTS check
 
         PermissionPolicy childPolicy = getParentService().isUpdateOnChildPermitted(getConsumer(), resource);
@@ -28,19 +36,19 @@ public abstract class ParentBaseRestController<R extends AbstractEntity, P exten
 
         getService().updateResourceById(resourceId, resource);
 
-        log.info("Processing [PUT] request with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"] successfully finished");
+        logProcessingFinished(HttpMethod.PUT, parentResourceId, resourceId);
         return new ResponseEntity<>(resource, HttpStatus.ACCEPTED);
     }
 
     public ResponseEntity<R> delete(final Integer parentResourceId, final Integer resourceId) throws ResourceNotFoundException, ResourceNotOwnedException {
-        log.info("Processing [DELETE] request with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"]");
+        logProcessingStarted(HttpMethod.DELETE, parentResourceId, resourceId);
         P parentResource = getParentService().getResourceById(parentResourceId); // EXISTS check
         R resource = getService().getResourceById(resourceId);
 
         PermissionPolicy childPolicy = getParentService().isDeleteOnChildPermitted(getConsumer(), resource);
         validatePermissionPolicy(parentResourceId, resource, getConsumer(), resourceId, childPolicy, HttpMethod.DELETE);
 
-        log.info("Processing [DELETE] request with parentResourceId ["+parentResourceId+"] and resourceId ["+resourceId+"] successfully finished");
+        logProcessingFinished(HttpMethod.DELETE, parentResourceId, resourceId);
         return new ResponseEntity(createDeletedMessage(), HttpStatus.ACCEPTED);
     }
 
